3b5389542c5dbf2b8d2e5b1e1f61476a48e1ed9a
Iam-tomiwa <ajayiemmanuel630@gmail.com>
2021-03-28 01:30:04 +0100

    fixed ui errors

--- dashboard/admin-js/dashboard.js
+++ dashboard/admin-js/dashboard.js
@@ -8,18 +8,19 @@ let navLinks = $$(".side-nav .fa.fa-times, .overlay");
 let bars = $(".fa.fa-bars");
 let overlay = $(".overlay");
 
-bars.addEventListener('click', _ => navFunc("block", "0"));
-Array.from(navLinks).forEach(link => link.addEventListener('click', _ => navFunc("none", "100%")));
+bars.addEventListener("click", _ => navFunc("block", "0"));
+Array.from(navLinks).forEach(link =>
+  link.addEventListener("click", _ => navFunc("none", "100%"))
+);
 
 function navFunc(type, val) {
-    nav.style.cssText = `clip-path: inset(0 ${val} 0 0);`;
-    overlay.style.display = type;
+  nav.style.cssText = `clip-path: inset(0 ${val} 0 0);`;
+  overlay.style.display = type;
 }
-
 
 /////////////////////////////
 // update UI
-let table = $('table tbody');
+let table = $("table tbody");
 let sortByAll = $(".filter-btn.all");
 let sortByDate = $(".filter-btn.date");
 let sortByName = $(".filter-btn.name");
@@ -27,108 +28,117 @@ let sortByCompany = $(".filter-btn.company");
 let sortBtn = Array.from($$(".filter-btn"));
 let pageCount = 1;
 let jsonData;
-let token = localStorage.getItem('oymg_token');
+let token = localStorage.getItem("oymg_token");
 
-
+// window.onload = e => {
+//   if (!localStorage.getItem("oymg_token")) {
+//     window.location.pathname = window.location.pathname.replace(
+//       "/dashboard/dashboard.html",
+//       "/dashboard/"
+//     );
+//   }
+// };
 
 // fetch function
 function fetchData() {
-    let url = `https://omwg.herokuapp.com/register?page=${pageCount}`;
-    return fetch(url, {
-        headers: {
-            "Authorization": `bearer ${token}`,
-        }
-    })
-        .then(res => res.json())
-        .then(json => json.data)
-        .catch(err => {
-            $(".oops").style.display = "flex";
-            console.log(err);
-        });
+  let url = `https://omwg.herokuapp.com/register?page=${pageCount}`;
+  return fetch(url, {
+    headers: {
+      Authorization: `bearer ${token}`,
+    },
+  })
+    .then(res => res.json())
+    .then(json => json.data)
+    .catch(err => {
+      $(".oops").style.display = "flex";
+      console.log(err);
+    });
 }
 
 // switch active class onclick of each sort button
-sortBtn.forEach(btn => btn.addEventListener('click', e => {
-    sortBtn.forEach(btn => btn.classList.remove('active'));
-    e.target.classList.add('active');
-}));
+sortBtn.forEach(btn =>
+  btn.addEventListener("click", e => {
+    sortBtn.forEach(btn => btn.classList.remove("active"));
+    e.target.classList.add("active");
+  })
+);
 
 // template for data
 function UIDatatemplate(user) {
-    let tr = document.createElement('tr');
-    tr.innerHTML = `<td>${user.firstName}</td>
+  let tr = document.createElement("tr");
+  tr.innerHTML = `<td>${user.firstName}</td>
                     <td>${user.lastName}</td>
                     <td>${user.email}</td>
                     <td>${user.companyName}</td>
                     <td>${user.jobTitle}</td>
                     <td>${user.createdAt.substr(0, 10)}</td>`;
-    table.appendChild(tr);
-    return tr.innerHTML;
+  table.appendChild(tr);
+  return tr.innerHTML;
 }
 
 // function to show default
 function showAll() {
-    fetchData()
-        .then(data => {
-            table.innerHTML = '';
-            data.registrants.forEach(registrant => {
-                UIDatatemplate(registrant);
-                jsonData = data;
-            });
-            if (data.registrants.length === 0) {
-                $(".oops").style.display = "flex";
-                $(".oops p").textContent = "page not found";
-            } else {
-                $(".oops").style.display = "none";
-            }
-        })
-        .catch(err => {
-            $(".oops").style.display = "flex";
-            console.log(err);
-        });
-    sortBtn.forEach(btn => btn.classList.remove('active'));
-    sortByAll.classList.add('active');
+  fetchData()
+    .then(data => {
+      table.innerHTML = "";
+      data.registrants.forEach(registrant => {
+        UIDatatemplate(registrant);
+        jsonData = data;
+      });
+      if (data.registrants.length === 0) {
+        $(".oops").style.display = "flex";
+        $(".oops p").textContent = "page not found";
+      } else {
+        $(".oops").style.display = "none";
+      }
+    })
+    .catch(err => {
+      $(".oops").style.display = "flex";
+      console.log(err);
+    });
+  sortBtn.forEach(btn => btn.classList.remove("active"));
+  sortByAll.classList.add("active");
 }
 
 // sort order for string type properties on json data
 function sortType(prop) {
-    return function (a, b) {
-        if (a[prop] > b[prop]) {
-            return 1;
-        } else if (a[prop] < b[prop]) {
-            return -1;
-        }
-        return 0;
-    };
+  return function (a, b) {
+    if (a[prop] > b[prop]) {
+      return 1;
+    } else if (a[prop] < b[prop]) {
+      return -1;
+    }
+    return 0;
+  };
 }
 
 // sort all string values
 function sortStr(prop) {
-    table.innerHTML = '';
-    jsonData.registrants
-        .sort(sortType(prop))
-        .forEach(registrant => {
-            UIDatatemplate(registrant);
-        });
+  table.innerHTML = "";
+  jsonData.registrants.sort(sortType(prop)).forEach(registrant => {
+    UIDatatemplate(registrant);
+  });
 }
 
 // sort by date
 function sortDate(prop) {
-    table.innerHTML = '';
-    jsonData.registrants
-        .sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
-        .forEach(registrant => {
-            UIDatatemplate(registrant);
-        });
+  table.innerHTML = "";
+  jsonData.registrants
+    .sort(
+      (a, b) =>
+        new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+    )
+    .forEach(registrant => {
+      UIDatatemplate(registrant);
+    });
 }
 
 // show and sort functions invoked
 showAll();
-sortByAll.addEventListener('click', showAll);
-sortByDate.addEventListener('click', sortDate);
-sortByName.addEventListener('click', _ => sortStr("firstName"));
-sortByCompany.addEventListener('click', _ => sortStr("companyName"));
-
+sortByAll.addEventListener("click", showAll);
+sortByDate.addEventListener("click", sortDate);
+sortByName.addEventListener("click", _ => sortStr("firstName"));
+sortByCompany.addEventListener("click", _ => sortStr("companyName"));
 
 // navigation among pages
 let next = $(".next");
@@ -137,67 +147,59 @@ let indicators = Array.from($$(".footer-links a"));
 let nextPrev = Array.from($$(".footer button"));
 let currentPage;
 
-next.addEventListener('click', (e) => {
-    pageCount == jsonData.totalPages ? pageCount = 1 : pageCount++;
-    currentPage = pageCount;
-    indicators.forEach(link => {
-        link.textContent = currentPage++
-        if (link.textContent == pageCount) {
-            link.classList.add('active')
-        } else {
-            link.classList.remove('active');
-        }
-    });
-    showAll();
-    console.log(`page: ${pageCount}`);
+next.addEventListener("click", e => {
+  pageCount == jsonData.totalPages ? (pageCount = 1) : pageCount++;
+  currentPage = pageCount;
+  indicators.forEach(link => {
+    link.textContent = currentPage++;
+    if (link.textContent == pageCount) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+  });
+  showAll();
+  console.log(`page: ${pageCount}`);
 });
 
-prev.addEventListener('click', (e) => {
-    pageCount === 1 ? pageCount = jsonData.totalPages : pageCount--;
-    indicators[0].textContent = pageCount;
-    currentPage = pageCount;
-    currentPage++;
-    for (let index = 1; index < indicators.length; index++) {
-        val = indicators[index].textContent;
-        indicators[index].textContent = currentPage++;
+prev.addEventListener("click", e => {
+  pageCount === 1 ? (pageCount = jsonData.totalPages) : pageCount--;
+  indicators[0].textContent = pageCount;
+  currentPage = pageCount;
+  currentPage++;
+  for (let index = 1; index < indicators.length; index++) {
+    val = indicators[index].textContent;
+    indicators[index].textContent = currentPage++;
+  }
+  indicators.forEach(link => {
+    if (link.textContent == pageCount) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
     }
-    indicators.forEach(link => {
-        if (link.textContent == pageCount) {
-            link.classList.add('active')
-        } else {
-            link.classList.remove('active');
-        }
-    })
-    showAll();
-    console.log(`page: ${pageCount}`);
+  });
+  showAll();
+  console.log(`page: ${pageCount}`);
 });
 
 // navigate to page clicked
-indicators.forEach(link => link.addEventListener('click', e => {
-    indicators.forEach(btn => btn.classList.remove('active'));
-    e.target.classList.add('active');
+indicators.forEach(link =>
+  link.addEventListener("click", e => {
+    indicators.forEach(btn => btn.classList.remove("active"));
+    e.target.classList.add("active");
     pageCount = e.target.textContent;
     showAll();
-}));
+  })
+);
 
 $(".download").addEventListener("click", e => {
-    fetch("https://omwg.herokuapp.com/register/download", {
-        method: 'GET',
-        headers: {
-            "Authorization": `bearer ${token}`,
-        }
-    })
-        .then(res => res)
-        .then(download => download)
-        .catch(err => console.log(err));
+  fetch("https://omwg.herokuapp.com/register/download", {
+    method: "GET",
+    headers: {
+      Authorization: `bearer ${token}`,
+    },
+  })
+    .then(res => res)
+    .then(download => download)
+    .catch(err => console.log(err));
 });
-
-
-
-
-
-
-
-
-
-
--- js/index.js
+++ js/index.js
@@ -2,28 +2,26 @@
 const $ = el => document.querySelector(el);
 const $$ = el => document.querySelectorAll(el);
 
-
 // code for count down begins here
 const countDownTimer = function (time) {
+  //2. calculate distance between curr time and deadline
+  let deadline = new Date(time).getTime();
+  let currTime = new Date().getTime();
+  let distTime = deadline - currTime;
 
-    //2. calculate distance between curr time and deadline
-    let deadline = new Date(time).getTime();
-    let currTime = new Date().getTime();
-    let distTime = deadline - currTime;
+  // calc the time left from deadline in days, hrs, mins, sec.
+  let secsLeft = Math.floor((distTime / 1000) % 60);
+  let minsLeft = Math.floor((distTime / (1000 * 60)) % 60);
+  let hrsLeft = Math.floor((distTime / (1000 * 60 * 60)) % 24);
+  let daysLeft = Math.floor(distTime / (1000 * 60 * 60 * 24));
 
-    // calc the time left from deadline in days, hrs, mins, sec.
-    let secsLeft = Math.floor((distTime / 1000) % 60);
-    let minsLeft = Math.floor((distTime / (1000 * 60)) % 60);
-    let hrsLeft = Math.floor((distTime / (1000 * 60 * 60)) % 24);
-    let daysLeft = Math.floor(distTime / (1000 * 60 * 60 * 24));
-
-    return {
-        secsLeft,
-        minsLeft,
-        hrsLeft,
-        daysLeft,
-        distTime
-    };
+  return {
+    secsLeft,
+    minsLeft,
+    hrsLeft,
+    daysLeft,
+    distTime,
+  };
 };
 let tLeft;
 // grab my time html elements to be updated here
@@ -33,27 +31,25 @@ const min = $(".time.min");
 const sec = $(".time.sec");
 
 const updateUITime = function () {
-    tLeft = countDownTimer('Dec 14, 2020 00:50:00 GMT+0000');
+  tLeft = countDownTimer("Dec 14, 2020 00:50:00 GMT+0000");
 
-    sec.innerHTML = ('0' + tLeft.secsLeft).slice(-2);
-    min.innerHTML = ('0' + tLeft.minsLeft).slice(-2);
-    hrs.innerHTML = ('0' + tLeft.hrsLeft).slice(-2);
-    days.innerHTML = ('0' + tLeft.daysLeft).slice(-2);
-
-}
+  sec.innerHTML = ("0" + tLeft.secsLeft).slice(-2);
+  min.innerHTML = ("0" + tLeft.minsLeft).slice(-2);
+  hrs.innerHTML = ("0" + tLeft.hrsLeft).slice(-2);
+  days.innerHTML = ("0" + tLeft.daysLeft).slice(-2);
+};
 
 updateUITime();
 let timeInterval = setInterval(() => {
-    updateUITime();
+  updateUITime();
 
-    if (tLeft.distTime <= 0) {
-        clearInterval(timeInterval);
-        sec.innerHTML = "00";
-        min.innerHTML = "00";
-        hrs.innerHTML = "00";
-        days.innerHTML = "00";
-    }
-
+  if (tLeft.distTime <= 0) {
+    clearInterval(timeInterval);
+    sec.innerHTML = "00";
+    min.innerHTML = "00";
+    hrs.innerHTML = "00";
+    days.innerHTML = "00";
+  }
 }, 1000);
 // countdown code ends here
 
@@ -62,25 +58,33 @@ let navbar = $(".nav2");
 let navLink = $$(".nav2 a");
 let openToggle = $(".fa.fa-bars");
 
-openToggle.addEventListener('click', _ => navbar.style.cssText = 'clip-path: circle(100%);');
-Array.from(navLink).forEach(link => link.addEventListener('click', _ => navbar.style.cssText = " clip-path: circle(0% at 100% 0);"));
-
+openToggle.addEventListener(
+  "click",
+  _ => (navbar.style.cssText = "clip-path: circle(100%);")
+);
+Array.from(navLink).forEach(link =>
+  link.addEventListener(
+    "click",
+    _ => (navbar.style.cssText = " clip-path: circle(0% at 100% 0);")
+  )
+);
 
 // change nav background on window scroll
-window.addEventListener('scroll', _ => {
-    let nav = $('.nav1');
-    let nav1Link = Array.from($$('.nav1 .navLink a, i.fa-bars'));
+window.addEventListener("scroll", _ => {
+  let nav = $(".nav1");
+  let nav1Link = Array.from($$(".nav1 .navLink a, i.fa-bars"));
 
-    if (window.pageYOffset == 0) {
-        nav.style.cssText = "background: transparent;";
-        nav1Link.forEach(link => link.style.color = 'white');
-    } else if (window.pageYOffset > 0) {
-        nav.style.cssText = "background: var(--bg-color);";
-        nav1Link.forEach(link => link.style.color = 'var(--text-color)');
-    }
-})
-
-
+  if (window.pageYOffset == 0) {
+    nav.style.cssText = "background: transparent;";
+    nav1Link.forEach(link => (link.style.cssText = "color: white;"));
+  } else if (window.pageYOffset > 0) {
+    nav.style.cssText = "background: var(--bg-color);";
+    nav1Link.forEach(
+      link =>
+        (link.style.cssText = "color: var(--text-color); border-color: black;")
+    );
+  }
+});
 
 // form display
 // let form = $('.registration-form');
@@ -99,5 +103,3 @@ window.addEventListener('scroll', _ => {
 //     form.style.transform = 'translate(-50%, -50%) scale(0)';
 //     overlay.style.display = 'none';
 // }
-
-
